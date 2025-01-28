0854ced960e96eb325a87b1da4bc41981a0a67fd
hungvp <hungvp@ecoit.com.vn>
2025-01-28 16:13:31 +0700

    update webpack to delete files in dist folder before building, and copy files in public folder to dist after building

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,6 +3,8 @@ var webpackMerge = require('webpack-merge');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 
@@ -17,6 +19,11 @@ module.exports = webpackMerge(commonConfig, {
   },
 
   plugins: [
+    new CleanWebpackPlugin(['dist'], { 
+      root: helpers.root(),
+      verbose: true,
+      dry: false
+    }),
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.optimize.UglifyJsPlugin(),
     new ExtractTextPlugin('[name].[hash].css'),
@@ -24,6 +31,9 @@ module.exports = webpackMerge(commonConfig, {
       'process.env': {
         'ENV': JSON.stringify(ENV)
       }
-    })
+    }),
+    new CopyWebpackPlugin([
+      { from: 'public', to: 'public' }
+    ])    
   ]
 });
